e388de9be7b1d3d18ba735e174ecb34c79b70ffd
gaoxinchuan <gaoxinchuan@zhihu.com>
2018-12-01 20:55:36 +0800

    添加file2

---
+++ Study/Study/ResetFile2.swift
@@ -0,0 +1,13 @@
+//
+//  ResetFile2.swift
+//  Study
+//
+//  Created by gaoxinchuan on 2018/12/1.
+//  Copyright © 2018年 gaoxinchuan. All rights reserved.
+//
+
+import UIKit
+
+class ResetFile2: NSObject {
+
+}
